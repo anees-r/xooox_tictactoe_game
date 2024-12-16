8ae651e379535f61ebfaf8ea5a4e6b869cfbfcac
anees-r <anees.dev2002@gmail.com>
2024-12-17 00:12:42 +0500

    First Commit

---
+++ lib/app_assets.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// colors
+var mainAccent = const Color(0xFF1d1923);
+
+// text styles
+TextStyle buttonText = const TextStyle();
+TextStyle playerText = const TextStyle();
+
+// icons
+var oIcon = "assets/icons/o.svg";
+var xIcon = "assets/icons/x.svg";
+
+// animations
+var xoooxAnimation = "assets/animations/xooox-animation.gif";
+
+// logos
+var xoooxLogo = "assets/logos/xooox-logo.png";
